7e90c5f2d8b59f908250ebfdaf2fd9defdd05abc
naganandareddy2657 <naganandareddy2657@gmail.com>
2023-01-16 22:22:22 +0530

    examples is added

---
+++ examples/src/main/java/examples/TestNumber.java
@@ -0,0 +1,19 @@
+package examples;
+
+public class TestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=21;
+		if(num>0)
+		System.out.println("it is positive");
+		else if(num<0)
+			System.out.println("it is negative");
+		else
+			System.out.println("it is Zero");
+		
+		
+	}
+
+}
